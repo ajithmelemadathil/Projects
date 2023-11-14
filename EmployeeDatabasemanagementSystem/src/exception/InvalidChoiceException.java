65746e3dcf5d51e87c74139246210526e27ea320
ajithmelemadathil <146378014+ajithmelemadathil@users.noreply.github.com>
2023-11-14 21:15:27 +0530

    Add files via upload

---
+++ EmployeeDatabasemanagementSystem/src/exception/InvalidChoiceException.java
@@ -0,0 +1,16 @@
+package exception;
+
+public class InvalidChoiceException extends RuntimeException {
+	String message;
+
+	public InvalidChoiceException(String message) {
+		
+		this.message = message;
+	}
+	@Override
+	public String getMessage() {
+		return message;
+	}
+	
+
+}
